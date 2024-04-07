c06be7b131be1b2a742651f4e476cf740b374cb9
yael shlanger <yaelshlanger@gmail.com>
2024-04-07 18:23:10 +0300

    THIRD COMMIT update get function

--- app.js
+++ app.js
@@ -1,43 +1,6 @@
-// const express = require('express')
-// const app = express()
-// const bodyParser=require('body-parser')
+const sdk = require('api')('@render-api/v1.0#34i64rhilu8ilhkj');
 
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json())
-// //יצירת מאזין
-// app.listen('3002', () => {
-//     console.log('my port working');
-// })
-// app.get('', (req, res) => {
-//        curl --request GET \
-//      --url 'https://api.render.com/v1/services?limit=20' \
-//      --header 'accept: application/json' \
-//      --header 'authorization: Bearer rnd_2DdcZPyACrDe1XgqN0WgoK5jpMSX'
-//     res.status(200).send('my sites')
-// })
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const port = 3000;
-
-app.get('/', async (req, res) => {
-    try {
-        const response = await axios.get('https://api.render.com/v1/services?limit=20', {
-            headers: {
-                'accept': 'application/json',
-                // הוסף את ה-API key שלך במקום הנקודות
-                'authorization': 'Bearer rnd_2DdcZPyACrDe1XgqN0WgoK5jpMSX'
-            }
-        });
-        // כאן אתה יכול להשתמש ב-response.data כדי לעשות משהו עם הנתונים שהתקבלו
-        res.status(200).json(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error fetching data from Render API');
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+sdk.auth('rnd_2DdcZPyACrDe1XgqN0WgoK5jpMSX');
+sdk.getServices({limit: '20'})
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
